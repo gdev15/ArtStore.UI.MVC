9565ffe6ebf9f501c643cfd83d2f252becf2b5cc
Gabriel Ramirez <gramirez@webgdesigns.com>
2023-11-19 20:29:38 -0600

    Add project files.

---
+++ ArtStore.UI.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ArtStore.UI.MVC.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
